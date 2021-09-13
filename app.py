c6b9ea7fc7fba4faec62e01bccf1f60f203bfd91
CH_Hwang <hch950627@naver.com>
2021-09-13 19:26:33 +0900

    진자 구조 나누기

--- app.py
+++ app.py
@@ -9,7 +9,19 @@ db = client.Yanudu
 
 @app.route('/')
 def main():
-    return render_template('asdf.html')
+    return render_template('home.html')
+
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
