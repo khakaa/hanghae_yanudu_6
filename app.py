83f663799ed65f45c95c8c3e7eeb40128832c85d
changonpark <changonpark@naver.com>
2021-09-13 19:33:58 +0900

    깃 허브 연습

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ db = client.Yanudu
 def main():
     return render_template('index.html')
 
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
