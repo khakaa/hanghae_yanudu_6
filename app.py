1d12b46f9961eb5e21e7534ee45adeac1d362ec7
CH_Hwang <hch950627@naver.com>
2021-09-13 17:42:43 +0900

    진자 예시

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ db = client.Yanudu
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('asdf.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-    #add commit push
