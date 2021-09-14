0caf49ed3adf419d2f70f0e5d5b2c86c8c317b6e
changonpark <changonpark@naver.com>
2021-09-14 09:13:50 +0900

    깃 허브

--- app.py
+++ app.py
@@ -18,16 +18,6 @@ def home():
 @app.route('/submit')
 def submit():
     return render_template('submit.html')
-<<<<<<< HEAD
-
-@app.route('/detail')
-def detail():
-    return render_template('detail.html')
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-=======
->>>>>>> c6b9ea7fc7fba4faec62e01bccf1f60f203bfd91
 
 @app.route('/detail')
 def detail():
