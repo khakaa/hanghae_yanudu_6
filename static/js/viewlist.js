75a10c0dab23fc1eb8858fcbeb31853eb6ead18e
CH_Hwang <hch950627@naver.com>
2021-09-15 17:20:47 +0900

    파일저장, 뷰 수정

--- static/js/viewlist.js
+++ static/js/viewlist.js
@@ -12,25 +12,26 @@ function Doc() {
 
             for (let i = 0; i < authors.length; i++) {
 
-                let name = authors[i]['name']
+                let title = authors[i]['title']
                 let url = authors[i]['url']
                 let content = authors[i]['content']
-                let img = authors[i]['img']
-                let like = authors[i]['like']
+                let file = authors[i]['file']
+                console.log(file)
+                let likes = authors[i]['likes']
 
                 let temp_html = `<div class="card mb-3 middle" style="max-width: 540px;">
                                     <div class="row g-0">
                                         <div class="col-md-4">
-                                            <img src="${img}" class="img-fluid rounded-start" alt="...">
+                                            <img src="../static/img/${file}" class="img-fluid rounded-start" alt="...">
                                         </div>
                                         <div class="col-md-8">
                                             <div class="card-body">
-                                                <h5 class="card-title">${name} (좋아요: ${like})<br></h5>
+                                                <h5 class="card-title">${title} (좋아요: ${likes})<br></h5>
                                                 <p class="card-text">${content}</p>
                                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                                    <button class="btn btn-primary me-md-2" onclick="likelist('${name}')" type="button">좋아요</button>
-                                                    <button class="btn btn-primary" onclick="deletelist('${name}')" type="button">싫어요</button>
+                                                    <button class="btn btn-primary me-md-2" onclick="likelist('${title}')" type="button">좋아요</button>
+                                                    <button class="btn btn-primary" onclick="deletelist('${title}')" type="button">싫어요</button>
                                                 </div>
                                             </div>
                                         </div>
